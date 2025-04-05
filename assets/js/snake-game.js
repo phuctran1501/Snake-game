6b980bbcc8d50a4f71a270186c54c8c40117eb6d
Phuc <trantrongphuc1501@gmail.com>
2025-04-05 11:00:54 +0700

    Added blueApples

--- assets/js/snake-game.js
+++ assets/js/snake-game.js
@@ -81,6 +81,7 @@ function resetGame() {
     gameOver = false;
     snake = new Snake(20, 20, 20);
     apple = gameMode === "multipleApples" ? new MultipleApples() : new Apple();
+    blueApples = [];  // Reset blue apples
     const gameOverElement = document.getElementById('gameOverText');
     if (gameOverElement) {
         gameOverElement.style.display = 'none';
@@ -113,6 +114,61 @@ function update() {
     checkCollisionWithBody();
     eatApple();
     checkHitWall();
+    createBlueApples(); // Tạo táo xanh dương nếu điều kiện thỏa mãn
+}
+
+let blueApples = []; // To store the blue apples
+function createBlueApples() {
+
+        if (snake.tail.length > 4 && blueApples.length === 0) { 
+            blueApples = [];
+            for (let i = 0; i < 2; i++) {
+                let newBlueApple;
+                do {
+                    newBlueApple = new Apple();
+                } while (isAppleTouchingSnake(newBlueApple)); 
+                newBlueApple.color = "blue"; 
+                blueApples.push(newBlueApple);
+            }
+        }
+}
+
+function isAppleTouchingSnake(apple) {
+    for (let i = 0; i < snake.tail.length; i++) {
+        if (apple.x === snake.tail[i].x && apple.y === snake.tail[i].y) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+function eatApple() {
+    let head = snake.tail[snake.tail.length - 1];
+    
+    for (let i = 0; i < blueApples.length; i++) {
+        if (blueApples[i].x === head.x && blueApples[i].y === head.y) {
+            snake.tail.splice(snake.tail.length - 2, 2); 
+            highScore -= 2; 
+            blueApples.splice(i, 1); 
+            break;
+        }
+    }
+
+    if (apple instanceof MultipleApples) {
+        for (let i = 0; i < apple.apples.length; i++) {
+            if (head.x === apple.apples[i].x && head.y === apple.apples[i].y) {
+                snake.tail.push({ x: apple.apples[i].x, y: apple.apples[i].y });
+                apple.apples.splice(i, 1);  
+                apple.apples.push(new Apple());  
+                break;
+            }
+        }
+    } else {
+        if (head.x === apple.x && head.y === apple.y) {
+            snake.tail.push({ x: apple.x, y: apple.y });
+            apple = new Apple();
+        }
+    }
 }
 
 function checkCollisionWithBody() {
@@ -169,26 +225,6 @@ function showGameOver() {
     clearInterval(gameLoopInterval);
 }
 
-function eatApple() {
-    let head = snake.tail[snake.tail.length - 1];
-    
-    if (apple instanceof MultipleApples) {
-        for (let i = 0; i < apple.apples.length; i++) {
-            if (head.x === apple.apples[i].x && head.y === apple.apples[i].y) {
-                snake.tail.push({ x: apple.apples[i].x, y: apple.apples[i].y });
-                apple.apples.splice(i, 1);  
-                apple.apples.push(new Apple());  
-                break;
-            }
-        }
-    } else {
-        if (head.x === apple.x && head.y === apple.y) {
-            snake.tail.push({ x: apple.x, y: apple.y });
-            apple = new Apple();
-        }
-    }
-}
-
 function checkHitWall() {
     let headTail = snake.tail[snake.tail.length - 1];
     if (gameSpeed == 50 || gameSpeed == 25) {
@@ -224,12 +260,17 @@ function draw() {
     canvasContext.font = "20px Arial";
     canvasContext.fillStyle = "#00FF42";
     canvasContext.fillText("Score: " + (snake.tail.length - 1), canvas.width - 100, 18);
+    
     if (apple instanceof MultipleApples) {
         for (let i = 0; i < apple.apples.length; i++) {
             createCircle(apple.apples[i].x + apple.apples[i].size / 2, apple.apples[i].y + apple.apples[i].size / 2, apple.apples[i].size / 2, apple.apples[i].color);
         }
     } else {
         createCircle(apple.x + apple.size / 2, apple.y + apple.size / 2, apple.size / 2, apple.color);
+    }
+
+    for (let i = 0; i < blueApples.length; i++) {
+        createCircle(blueApples[i].x + blueApples[i].size / 2, blueApples[i].y + blueApples[i].size / 2, blueApples[i].size / 2, blueApples[i].color);
     }
 }
 
@@ -252,13 +293,16 @@ window.addEventListener("keydown", (event) => {
         if (event.keyCode === 37 && snake.rotateX !== 1) {
             snake.rotateX = -1;
             snake.rotateY = 0;
-        } else if (event.keyCode === 38 && snake.rotateY !== 1) {
+        }
+        if (event.keyCode === 38 && snake.rotateY !== 1) {
             snake.rotateX = 0;
             snake.rotateY = -1;
-        } else if (event.keyCode === 39 && snake.rotateX !== -1) {
+        }
+        if (event.keyCode === 39 && snake.rotateX !== -1) {
             snake.rotateX = 1;
             snake.rotateY = 0;
-        } else if (event.keyCode === 40 && snake.rotateY !== -1) {
+        }
+        if (event.keyCode === 40 && snake.rotateY !== -1) {
             snake.rotateX = 0;
             snake.rotateY = 1;
         }
@@ -355,3 +399,4 @@ class MultipleApples {
 
 let snake = new Snake(20, 20, 20);
 let apple = new Apple();
+let blueApple = null;  // Táo xanh dương
